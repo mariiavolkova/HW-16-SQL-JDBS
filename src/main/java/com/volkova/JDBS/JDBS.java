18f94b54a162f5b1fd405247d896843b9ca1c770
mariiavolkova <67053060+mariiavolkova@users.noreply.github.com>
2020-08-24 18:15:21 +0300

    Delete JDBS.java

--- src/main/java/com/volkova/JDBS/JDBS.java
+++
@@ -1,9 +0,0 @@
-package com.volkova.JDBS;
-
-public class JDBS {
-    public static final String JDBC_DRIVER = "org.h2.Driver";
-    public static final String DB_URL = "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1";
-
-    public static final String USER = "sa";
-    public static final String PASS = "";
-}
